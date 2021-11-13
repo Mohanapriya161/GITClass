b09a67c355ef0311066dfa44a57c4d68d94a3b01
Mohanapriya161 <vmohanapriya50gmail.com>
2021-11-13 19:17:41 +0100

    Latest code

--- src/test/java/org/hcl/Priya.java
+++ src/test/java/org/hcl/Priya.java
@@ -2,4 +2,18 @@ package org.hcl;
 
 public class Priya {
 
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	
+   private void method3() {
+	// TODO Auto-generated method stub
+
+}
 }
